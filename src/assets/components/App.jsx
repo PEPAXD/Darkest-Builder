4dd3d41177555bf42913207acf3144932fd41820
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-02-21 16:18:52 -0300

    addLogoAndParraf

--- src/assets/components/App.jsx
+++ src/assets/components/App.jsx
@@ -1,4 +1,3 @@
-import './styles/App.css'
 import Home from './Home'
 
 function App() {
--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -4,10 +4,21 @@ import "./styles/Home.scss";
 export default function Home() {
   return (
     <div className="background">
-        <div className="snow"></div>
-        <div className="title">
-            <h1>Darkest Builder</h1>
-        </div>
+      <div className="snow"></div>
+      <div className="containTittle">
+        <img src="dd2-logo.png" alt="" />
+        <p>
+          Explora el mundo de Darkest Dungeon 2 con Darkest Builder. Descubre
+          información detallada de héroes, fuerzas, debilidades,
+          estadísticas y subclases. 
+          <br />
+          Prueba distintos estilos de juego,  y sigue nuestra tierlist de
+          héroes y composiciones de equipo.
+          <br />
+          Domina la oscuridad de la mazmorra con nuestras guías basadas en la wikioficial del juego.
+        </p>
+        <button>Build Hero</button>
+      </div>
     </div>
   );
 }
