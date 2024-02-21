061ad25a9c6bb4e1f41a63eae7442ed7feefc229
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-02-21 18:57:13 -0300

    Add Footer

--- src/assets/components/App.jsx
+++ src/assets/components/App.jsx
@@ -1,11 +1,19 @@
-import Home from './Home'
+import Home from "./Home";
 
 function App() {
   return (
     <>
       <Home />
+      <footer>
+        <div>
+          © 2024 Darkest Builder &nbsp;
+          <a href="https://github.com/PEPAXD">
+             Developed By Mauro Pepa
+          </a>
+        </div>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -5,19 +5,33 @@ export default function Home() {
   return (
     <div className="background">
       <div className="snow"></div>
-      <div className="containTittle">
+      <div className="homeContain">
         <img src="dd2-logo.png" alt="" />
-        <p>
-          Explora el mundo de Darkest Dungeon 2 con Darkest Builder. Descubre
-          información detallada de héroes, fuerzas, debilidades,
-          estadísticas y subclases. 
-          <br />
-          Prueba distintos estilos de juego,  y sigue nuestra tierlist de
-          héroes y composiciones de equipo.
-          <br />
-          Domina la oscuridad de la mazmorra con nuestras guías basadas en la wikioficial del juego.
-        </p>
-        <button>Build Hero</button>
+
+        <div class="welcomeContain">
+          <div class="welcometitle">
+            Welcome To The Darkest Builder
+            <hr />
+          </div>
+
+          <div class="welcomebody">
+
+              <p>
+                Explora información detallada sobre héroes, sus habilidades,
+                fortalezas, debilidades, estadísticas y subclases.
+              </p>
+              <p>
+                Experimenta diferentes estilos de juego y sigue nuestra tier
+                list de héroes y composiciones de equipo.
+              </p>
+              <p>
+                Domina la oscuridad de la mazmorra con nuestras guías, basadas
+                en la wikioficial del juego.
+              </p>
+
+            <button>Build Hero</button>
+          </div>
+        </div>
       </div>
     </div>
   );
