8108bd936fd5fdc71114e99e5fd553d5b0d615b9
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-02-27 17:04:36 -0300

    wip-Header

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,16 +1,21 @@
 import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 function Header() {
   return (
     <header>
-      <Container>
-        <Navbar expand="lg">
-          <Container>
-            <Navbar.Brand href="#" >Darkest Builder</Navbar.Brand>
-          </Container>
-        </Navbar>
-      </Container>
+      <Navbar.Brand href="#home">Darkest Builder</Navbar.Brand>
+      <Nav className="navBar">
+        <Container>
+        <Nav.Link href="#About">About</Nav.Link>
+        <Nav.Link href="#GameMap">GameMap</Nav.Link>
+        <Nav.Link href="#Enemies">Enemies</Nav.Link>
+        <Nav.Link href="#HeroBuilds">Hero-Builds</Nav.Link>
+        <Nav.Link href="#Backstories">Backstories</Nav.Link>
+        <Nav.Link href="#Tierlist">TierList-Game</Nav.Link>
+        </Container>
+      </Nav>
     </header>
   );
 }
