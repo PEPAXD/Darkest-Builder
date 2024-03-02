696c0adc708e1ff23762b0722a9eab46ee4e276a
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-03-02 19:57:33 -0300

    addWikiGameButton

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,14 +1,19 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function Header() {
   return (
     <header>
-      <Navbar.Brand href="#" className="navBrand">
-        Darkest Builder
-      </Navbar.Brand>
+      <h1>My Website</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
+      </nav>
 
     </header>
   );
--- src/assets/components/HomePage.jsx
+++ src/assets/components/HomePage.jsx
@@ -1,25 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./styles/snowAnimation.scss";
+import "./styles/HomePage.scss";
+
+import { SiFandom } from "react-icons/si";
 
 export default function HomePage({ url_Bg }) {
-
-  const [src, setSrc] = useState('dd2-logo.png');
+  const [src, setSrc] = useState("dd2-logo.png");
 
   useEffect(() => {
     function handleResize() {
       if (window.innerWidth < 800) {
-        setSrc('dd2-logoResponsive.png');
+        setSrc("dd2-logoResponsive.png");
       } else {
-        setSrc('dd2-logo.png');
+        setSrc("dd2-logo.png");
       }
     }
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize();
-    
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div className="Home">
@@ -27,11 +28,24 @@ export default function HomePage({ url_Bg }) {
         className="background"
         style={{ background: `url(${url_Bg}) no-repeat center top/cover` }}
       >
+        {/* TempDisable 
         <div className="snow"></div>
-
+        */}
         <div className="homeContain">
-          <img src={src} alt=""/> 
+          <img src={src} alt="" />
         </div>
+
+        <div className="FooterButtons">
+          <ul class="wrapper">
+            <li class="icon WikiFandom">
+              <span class="tooltip">WikiGame</span>
+              <span>
+                <SiFandom />
+              </span>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </div>
   );
