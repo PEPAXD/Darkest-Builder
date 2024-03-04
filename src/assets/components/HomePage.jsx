0c3e76954fc0e6dd4bf90b858501b2e72791db8a
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-03-04 00:19:38 -0300

    optimize code HomePageButtonLinks

--- src/assets/components/HomePage.jsx
+++ src/assets/components/HomePage.jsx
@@ -6,23 +6,33 @@ import { SiFandom } from "react-icons/si";
 import { IoLogoGameControllerB } from "react-icons/io";
 import { FaBookReader } from "react-icons/fa";
 
-export default function HomePage({ url_Bg }) {
-  const [src, setSrc] = useState("dd2-logo.png");
+const useWindowWidth = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    function handleResize() {
-      if (window.innerWidth < 800) {
-        setSrc("dd2-logoResponsive.png");
-      } else {
-        setSrc("dd2-logo.png");
-      }
-    }
+    const handleResize = () => setWindowWidth(window.innerWidth);
 
     window.addEventListener("resize", handleResize);
-    handleResize();
-
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  return windowWidth;
+};
+
+const FooterButton = ({ href, iconColor, fontSize, Icon, bannerName }) => (
+  <a href={href} target="_blank" rel="noopener noreferrer">
+    <li className="icon" id={iconColor}>
+      <span style={{ fontSize }}>
+        <Icon />
+      </span>
+    </li>
+    <span className="tooltip">{bannerName}</span>
+  </a>
+);
+
+export default function HomePage({ url_Bg }) {
+  const windowWidth = useWindowWidth();
+  const src = windowWidth < 800 ? "dd2-logoResponsive.png" : "dd2-logo.png";
 
   return (
     <div className="Home">
@@ -30,53 +40,33 @@ export default function HomePage({ url_Bg }) {
         className="background"
         style={{ background: `url(${url_Bg}) no-repeat center top/cover` }}
       >
-        {/* Snow Animation
-        <div className="snow"></div> */}
-
         <div className="homeContain">
           <img src={src} alt="" />
         </div>
 
         <div className="FooterButtons">
-          <ul class="wrapper">
-            <a
+          <ul className="wrapper">
+            <FooterButton
               href="https://darkestdungeon.fandom.com/wiki/Darkest_Dungeon_II_Wiki"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <li class="icon WikiFandom">
-                <span style={{ fontSize: "1.4em" }}>
-                  <SiFandom />
-                </span>
-              </li>
-              <span class="tooltip">WikiGame</span>
-            </a>
-
-            <a
+              iconColor="wikiFandom"
+              fontSize="1.4em"
+              Icon={SiFandom}
+              bannerName="WikiGame"
+            />
+            <FooterButton
               href="https://steamcommunity.com/sharedfiles/filedetails/?id=2973257745"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <li class="icon steamGuide">
-                <span style={{ fontSize: "1.3em" }}>
-                  <FaBookReader />
-                </span>
-              </li>
-              <span class="tooltip">SteamGuide</span>
-            </a>
-
-            <a
+              iconColor="steamGuide"
+              fontSize="1.3em"
+              Icon={FaBookReader}
+              bannerName="SteamGuide"
+            />
+            <FooterButton
               href="https://www.darkestdungeon.com/darkest-dungeon-2/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <li class="icon gameWeb">
-                <span style={{ fontSize: "1.6em" }}>
-                  <IoLogoGameControllerB />
-                </span>
-              </li>
-              <span class="tooltip">GameWeb</span>
-            </a>
+              iconColor="iconGameWeb"
+              fontSize="1.6em"
+              Icon={IoLogoGameControllerB}
+              bannerName="GameWeb"
+            />
           </ul>
         </div>
       </div>
