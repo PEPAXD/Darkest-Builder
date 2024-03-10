458a9614476449c4548029cdc8a07515b8e39325
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-03-10 15:31:31 -0300

    optimize codeAbout

--- src/assets/components/AboutPage.jsx
+++ src/assets/components/AboutPage.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import { aboutData } from "../data/aboutSection";
 import "./styles/AboutPage.scss";
 
 export default function AboutPage({ url_Bg }) {
-  const [selectedSection, setSelectedSection] = useState("A");
+  const [selectedRadio, setSelectedRadio] = useState(0);
 
-  const handleChange = (event) => {
-    setSelectedSection(event.target.value);
+  const handleRadioChange = (index) => {
+    setSelectedRadio(index);
   };
 
   return (
@@ -16,93 +17,31 @@ export default function AboutPage({ url_Bg }) {
       >
         <div className="AboutContainer">
           <div className="AboutContent">
-            <h3>WELCOME TO THE DARKEST BUILDER</h3>
-            <br />
-            <p>
-              Bienvenido a Darkest Builder, tu guia para perfeccionar tus
-              estrategias en el peligroso mundo de Darkest Dungeon. <br />
-              <br />
-              Este sitio web ha sido diseñado con el objetivo de proporcionar a
-              los jugadores una guía exhaustiva y especializada en la creación
-              de equipos para enfrentar los desafíos más oscuros y peligrosos.
-            </p>
-            <br />
-            <p>
-              La guia esta pensada para jugadores de todos los niveles, ya seas
-              un aventurero novato que se adentra por primera vez en las
-              inhóspitas mazmorras o un veterano que busca nuevas estrategias,
-              Darkest Builder es tu compañero indispensable.
-              <br />
-              <br />
-              Prepara tu equipo, desentraña los secretos de la oscuridad y
-              triunfa sobre las fuerzas que amenazan con sumirte en la locura.
-              <br />
-              <br />
-              ¡Explora Darkest Builder hoy y forja tu camino hacia la victoria
-              en Darkest Dungeon!
-            </p>
+            <h2>{aboutData[selectedRadio].title}</h2>
+
+            <div>
+              {aboutData[selectedRadio].content.map((paragraph, i) => (
+                <p key={i}>{paragraph}</p>
+              ))}
+            </div>
           </div>
+
           <div className="PointSections">
-            <label>
-              <input
-                type="radio"
-                name="section"
-                value="A"
-                checked={selectedSection === "A"}
-                onChange={handleChange}
-                style={{ display: "none" }}
-              />
-              <img
-                src="public/icon/aboutGame.jpg"
-                alt=""
-                className="radio-image"
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="section"
-                value="B"
-                checked={selectedSection === "B"}
-                onChange={handleChange}
-                style={{ display: "none" }}
-              />
-              <img
-                src="public/icon/mapGame.jpg"
-                alt=""
-                className="radio-image"
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="section"
-                value="C"
-                checked={selectedSection === "C"}
-                onChange={handleChange}
-                style={{ display: "none" }}
-              />
-              <img
-                src="public/icon/enemiesGame.jpg"
-                alt=""
-                className="radio-image"
-              />
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="section"
-                value="D"
-                checked={selectedSection === "D"}
-                onChange={handleChange}
-                style={{ display: "none" }}
-              />
-              <img
-                src="public/icon/heroesGame.jpg"
-                alt=""
-                className="radio-image"
-              />
-            </label>
+            {aboutData.map((item, index) => (
+              <label key={index}>
+                <input
+                  type="radio"
+                  value={index}
+                  checked={selectedRadio === index}
+                  onChange={() => handleRadioChange(index)}
+                  style={{ display: "none" }}
+                />
+                <img
+                  src={item.imgRadio}
+                  className="radio-image"
+                />
+              </label>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/assets/data/aboutSection.js
@@ -0,0 +1,20 @@
+// data.js
+export const aboutData = [
+    {
+      title: 'WELCOME TO THE DARKEST BUILDER',
+      imgRadio: 'public/icon/aboutGame.jpg',
+      content: [
+        'Bienvenido a Darkest Builder, tu guia para perfeccionar tus estrategias en el peligroso mundo de Darkest Dungeon.',
+        'Este sitio web ha sido diseñado con el objetivo de proporcionar a los jugadores una guía exhaustiva y especializada en la creación de equipos para enfrentar los desafíos más oscuros y peligrosos.',
+        'La guia esta pensada para jugadores de todos los niveles, ya seas un aventurero novato que se adentra por primera vez en las inhóspitas mazmorras o un veterano que busca nuevas estrategias, Darkest Builder es tu compañero indispensable.',
+        'Prepara tu equipo, desentraña los secretos de la oscuridad y triunfa sobre las fuerzas que amenazan con sumirte en la locura.',
+        '¡Explora Darkest Builder hoy y forja tu camino hacia la victoria en Darkest Dungeon!'
+      ]
+    },
+
+    {
+        title: 'text BLA BLA BLA',
+        imgRadio: 'public/icon/mapGame.jpg',
+        content: ['Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.']
+    }
+  ];
